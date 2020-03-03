568ab4af0f8f93646592444f904d59e3f46a263c
GrandpaCardigan <nicholas.k.kier@gmail.com>
2020-03-02 21:18:12 -0500

    Thinking emoji

--- src/Merge.java
+++ src/Merge.java
@@ -6,34 +6,42 @@ public class Merge {
     public static void main(String[] args) throws FileNotFoundException {
         File fileOne = new File(args[0]);
         File fileTwo = new File(args[1]);
-        Scanner kbs = new Scanner(fileOne);
-        int[] fileInputOne = new int[500];
+        Scanner scanner = new Scanner(fileOne);
 
 
         int fileCountOne = 0;
-        while (kbs.hasNext()) {
-            fileInputOne[fileCountOne] = kbs.nextInt();
+        int[] fileInputOne = new int[500];
+        while (scanner.hasNext()) {
+            fileInputOne[fileCountOne] = scanner.nextInt();
             fileCountOne++;
         }
         int[] integerArrayOne = new int[fileCountOne];
-        System.arraycopy(fileInputOne, 0, integerArrayOne, 0, fileCountOne);
+        System.arraycopy(fileInputOne,
+                0,
+                integerArrayOne,
+                0,
+                fileCountOne);
 
-        kbs = new Scanner(fileTwo);
+        scanner = new Scanner(fileTwo);
 
         int fileCountTwo = 0;
         int[] fileInputTwo = new int[500];
-        while (kbs.hasNext()) {
-            fileInputTwo[fileCountTwo] = kbs.nextInt();
+        while (scanner.hasNext()) {
+            fileInputTwo[fileCountTwo] = scanner.nextInt();
             fileCountTwo++;
         }
 
         int[] integerArrayTwo = new int[fileCountTwo];
-        System.arraycopy(fileInputTwo, 0, integerArrayTwo, 0, fileCountTwo);
+        System.arraycopy(fileInputTwo,
+                0,
+                integerArrayTwo,
+                0,
+                fileCountTwo);
 
         int i;
         int j;
-        int n = integerArrayOne.length;
-        for (i = 1; i < n; i++) {
+        int arrayLength = integerArrayOne.length;
+        for (i = 1; i < arrayLength; i++) {
             int startingInt = integerArrayOne[i];
             j = i - 1;
             while (j >= 0 && integerArrayOne[j] > startingInt) {
@@ -42,10 +50,13 @@ public class Merge {
             }
             integerArrayOne[j + 1] = startingInt;
         }
+        System.out.print(integerArrayOne[0]);
+        for (i = 1; i < arrayLength; i++)
+            System.out.print(" " + integerArrayOne[i]);
+        System.out.print("\n");
 
-
-        int m = integerArrayTwo.length;
-        for (i = 1; i < m; i++) {
+        int arrayTwoLength = integerArrayTwo.length;
+        for (i = 1; i < arrayTwoLength; i++) {
             int initial = integerArrayTwo[i];
             j = i - 1;
             while (j >= 0 && integerArrayTwo[j] < initial) {
@@ -54,35 +65,31 @@ public class Merge {
             }
             integerArrayTwo[j + 1] = initial;
         }
+        System.out.print(integerArrayTwo[0]);
+        for (i = 1; i < arrayTwoLength; i++) {
+            System.out.print(" " + integerArrayTwo[i]);
+        }
+        System.out.print("\n");
 
         int[] integerArrayThree = new int[fileCountOne + fileCountTwo];
-        i = n - 1;
+        i = arrayLength - 1;
         j = 0;
         int k = 0;
 
-        while (i >= 0 && j < m) if (integerArrayOne[i] > integerArrayTwo[j])
+        while (i >= 0 && j < arrayTwoLength) if (integerArrayOne[i] > integerArrayTwo[j])
             integerArrayThree[k++] = integerArrayOne[i--];
         else
             integerArrayThree[k++] = integerArrayTwo[j++];
 
         while (i >= 0) integerArrayThree[k++] = integerArrayOne[i--];
 
-        while (j < m) {
+        while (j < arrayTwoLength) {
             integerArrayThree[k++] = integerArrayTwo[j++];
         }
-
-        System.out.print(integerArrayOne[0]);
-        for (i = 1; i < n; i++)
-            System.out.print(" " + integerArrayOne[i]);
-        System.out.print("\n");
-        System.out.print(integerArrayTwo[0]);
-        for (i = 1; i < m; i++) {
-            System.out.print(" " + integerArrayTwo[i]);
-        }
-        System.out.print("\n");
 
         System.out.print(integerArrayThree[0]);
         for (i = 1; i < k; i++)
             System.out.print(" " + integerArrayThree[i]);
+        System.out.print("\n");
     }
 }
